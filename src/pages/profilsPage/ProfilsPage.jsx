75152585bbe19868e3906d3d98e973625660e163
NeyyTe <maxxence.marechal@gmail.com>
2024-05-07 05:06:46 +0200

    Add link retour accueil et divider entre les navLinks de la sidebar

--- src/pages/profilsPage/ProfilsPage.jsx
+++ src/pages/profilsPage/ProfilsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-
+import { Divider } from "@nextui-org/divider";
 function ProfilsPage() {
   const profiles = [
     "Mes paramètres",
@@ -14,7 +14,7 @@ function ProfilsPage() {
     <>
       <div className="flex  ">
         <div className=" flex flex-col  bg-primary-50/70  *:text-xl w-1/5 justify-between p-8 py-12">
-          <div className="flex flex-col gap-8 justify-items-center  ">
+          <div className="flex flex-col  justify-items-center  ">
             {profiles.map((profile) => (
               <NavLink
                 className={({ isActive }) => {
@@ -24,6 +24,7 @@ function ProfilsPage() {
                 to={`/profil/${profile}`}
               >
                 <p className="">{profile}</p>
+                <Divider className="my-8 bg-gradient-to-r from-purple-500/10 via-fuchsia-500 to-violet-800/10" />
               </NavLink>
             ))}
           </div>
